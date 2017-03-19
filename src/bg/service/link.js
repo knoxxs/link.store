2ea3ffaf6d6244a421d06772641afe7132972b65
Abhishek Gupta <abhi.bansal21@gmail.com>
2017-03-19 16:41:07 +0530

    adds repo code to link service

--- src/bg/service/link.js
+++ src/bg/service/link.js
@@ -1,7 +1,11 @@
 (function (ls, $) {
-    ls.service.link = {
-        save: function () {
+    var type = "link";
 
+    ls.service.link = {
+        save: function (link) {
+            //todo add business logic
+
+            ls.repo.save(type, link);
         },
         remove: function () {
 
