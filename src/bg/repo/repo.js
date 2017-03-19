9e863ae8377e6cd678e677a6f836116dcc8d0c2d
Abhishek Gupta <abhi.bansal21@gmail.com>
2017-03-19 16:37:47 +0530

    adds partial base to repo

--- src/bg/repo/repo.js
+++ src/bg/repo/repo.js
@@ -1,3 +1,19 @@
 (function (ls, $) {
+    function _checkAndGetStore(type) {
+        var typeStore = localStorage[type];
+        if (!typeStore) {
+            typeStore = JSON.stringify([]);
+            localStorage[type] = typeStore;
+        }
+
+        return typeStore;
+    }
+
+    function save(type, data) {
+        var store = _checkAndGetStore(type);
+        store.push(data);
+    }
+
     ls.repo = {};
+    ls.repo.save = save;
 }(ls, $));
