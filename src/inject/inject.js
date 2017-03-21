263be7deef6bdb6530548323a9df215293d2d4ec
Abhishek Gupta <abhi.bansal21@gmail.com>
2017-03-22 02:36:04 +0530

    add basic code to detech whenever user searches on google.

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,1 +1,17 @@
-console.log("Hello. This message was sent from scripts/inject.js");
+var searchBoxSelector = 'input#lst-ib.gsfi';
+
+var isSearchBoxLoadedCheckInterval = setInterval(bindSearchBoxChange, 100);
+function bindSearchBoxChange() {
+    var jSearchBox = $(searchBoxSelector);
+
+    if (jSearchBox && jSearchBox.length) {
+        clearInterval(isSearchBoxLoadedCheckInterval);
+
+        jSearchBox.bind('change', onSearch);
+    }
+}
+
+function onSearch() {
+    //console.log("val: " + $(this).val());
+    console.log("val: " + this.value);
+}
