c5f0a5186b398710ae30f38337dc92777de1edb5
Abhishek Gupta <abhi.bansal21@gmail.com>
2017-03-24 02:32:41 +0530

    adds hack because .constructor is getting called

--- src/bg/model/linkModel.js
+++ src/bg/model/linkModel.js
@@ -1,5 +1,14 @@
 ls.model.link = function (id, url, visitCount) {
-    this.id = id;
-    this.url = url;
-    this.visitCount = visitCount;
+    if (this instanceof ls.model.link) {
+        if (this.id || this.url || this.visitCount) {
+            return new ls.model.link(id, url, visitCount);
+        } else {
+            this.id = id;
+            this.url = url;
+            this.visitCount = visitCount;
+        }
+    }
+    else {
+        return new ls.model.link(id, url, visitCount);
+    }
 };
